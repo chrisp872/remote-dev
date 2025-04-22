b1b2d45d4599ef6609ef15a1a7867c0be7a35f5e
Chris Parent <chris.parent@outlook.com>
2025-04-22 18:30:03 -0400

    WIP.  Commit to move to another computer.

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,14 +1,18 @@
-export default function SearchForm() {
+export default function SearchForm({searchtext, setSearchText}) {
+
   return (
     <form onSubmit={e => {
       e.preventDefault();
       
-    }} action="#" className="search">
+    }}
+     action="#" className="search">
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchtext}
+        onChange={e => {setSearchText(e.target.value)}}
         spellCheck="false"
         type="text"
         required
